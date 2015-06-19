66463bf61c3522d47719efb55feae1ef5f558ded
Ron Waldon <jokeyrhyme@gmail.com>
2015-06-19 17:03:33 +1000

    extract functionality from legacy project

--- index.js
+++ index.js
@@ -1,3 +1,121 @@
 'use strict';
 
-module.exports = null;
+// foreign modules
+
+var clone = require('cyclonejs').clone;
+
+// this module
+
+/**
+* @private
+* @param {Object} target the object to which properties will be added
+* @param {Object} source the object from which properties will be copied
+* @returns {Object} target
+*/
+function extend (target, source) {
+  if (!target || typeof target !== 'object') {
+    throw new TypeError('1st argument must be an Object');
+  }
+  if (!source || typeof source !== 'object') {
+    throw new TypeError('1st argument must be an Object');
+  }
+  Object.keys(source).forEach(function (prop) {
+    var value = source[prop];
+    if (typeof value === 'string') {
+      if (value) {
+        target[prop] = value.trim();
+      }
+    } else {
+      target[prop] = value;
+    }
+  });
+  return target;
+}
+
+/**
+* @private
+* @param {Objects[]} objects collection of [ { name: '...', ... }, ... ]
+* @param {String[]} names desired ordering / filtering of objects
+* @returns {Objects[]} sorted and filtered objects
+*/
+function sortAndFilterByName (objects, names) {
+  var result;
+
+  // remove all elements not needed for this variation
+  result = objects.filter(function (o) {
+    return names.indexOf(o.name) !== -1;
+  });
+  // sort elements as per the variation-specific order
+  result.sort(function (a, b) {
+    var aIndex, bIndex;
+    aIndex = names.indexOf(a.name);
+    bIndex = names.indexOf(b.name);
+    return aIndex - bIndex;
+  });
+
+  return result;
+}
+
+/**
+ * @public
+ * @param {Object} def original definition with multiple variations
+ * @param {String} variation the specific variation desired
+ * @return {Object} definition for a single variation
+ */
+function flattenDefinition (def, variation) {
+  function flattenComponents (d) {
+    var attrs = d['default'] || {};
+    if (variation && d[variation]) {
+      extend(attrs, d[variation]);
+    }
+    return attrs;
+  }
+
+  if (!def || typeof def !== 'object') {
+    throw new TypeError('1st argument should be an Object');
+  }
+  if (!def['default']) {
+    throw new TypeError('Object is missing the "default" property');
+  }
+  if (!def['default'].name || typeof def['default'].name !== 'string') {
+    throw new TypeError('"default" Object is missing the "name" String');
+  }
+
+  // clone the definition object first, for safety
+  def = clone(def);
+
+  // found definition, but need to collapse to specific variation/view
+  [
+  '_elements',
+  '_sections',
+  '_pages',
+  '_behaviours',
+  '_checks',
+  '_actions'
+  ].forEach(function (components) {
+    if (Array.isArray(def['default'][components])) {
+      def['default'][components] = def['default'][components].map(flattenComponents);
+    }
+  });
+
+  if (!variation) {
+    // no further merging required
+    return def['default'];
+  }
+
+  /*eslint-disable no-underscore-dangle*/ // "_elements" is part of our spec
+  if (def[variation]) {
+    if (Array.isArray(def[variation]._elements)) {
+      def['default']._elements = sortAndFilterByName(def['default']._elements, def[variation]._elements);
+      delete def[variation]._elements;
+    }
+    extend(def['default'], def[variation]);
+  }
+  /*eslint-enable no-underscore-dangle*/
+
+  return def['default'];
+}
+
+module.exports = {
+  flatten: flattenDefinition
+};
--- test/index.js
+++ test/index.js
@@ -14,9 +14,211 @@ require('tape-chai');
 
 test('variedDefinition', function (t) {
 
-  t.test('is defined', function (tt) {
-    tt.isDefined(variedDefinition);
+  t.test('is an Object', function (tt) {
+    tt.isObject(variedDefinition);
     tt.end();
   });
 
+});
+
+test('variedDefinition.flatten', function (t) {
+
+  t.test('flattenDefinition is a function', function (tt) {
+    tt.isFunction(variedDefinition.flatten);
+    tt.end();
+  });
+
+  t.test('throws an error for bad argument[0]', function (tt) {
+    tt['throws'](function () {
+      variedDefinition.flatten(null);
+    }, Error);
+    tt['throws'](function () {
+      variedDefinition.flatten({});
+    }, Error);
+    tt['throws'](function () {
+      variedDefinition.flatten({ 'default': {} });
+    }, Error);
+    tt.end();
+  });
+
+  t.end();
+});
+
+test('flattening', function (t) {
+  var def, flat;
+
+  def = {
+    'default': {
+      name: 'my-form',
+      _elements: [
+        {
+          'default': {
+            name: 'my-element'
+          }
+        }
+      ]
+    }
+  };
+  flat = variedDefinition.flatten(def);
+
+  t.test('output is an Object', function (tt) {
+    tt.isObject(flat);
+    tt.end();
+  });
+
+  t.test('properties in default are flattened as expected', function (tt) {
+    tt.notProperty(flat, 'default');
+    tt.isString(flat.name);
+    tt.equal(flat.name, def['default'].name);
+    tt.end();
+  });
+
+  t.test('components in default are flattened as expected', function (tt) {
+    var el;
+    tt.isArray(flat._elements);
+    tt.lengthOf(flat._elements, 1);
+    el = flat._elements[0];
+    tt.notProperty(el, 'default');
+    tt.isString(el.name);
+    tt.equal(el.name, def['default']._elements[0]['default'].name);
+    tt.end();
+  });
+
+  t.end();
+});
+
+test('flattening with variations', function (t) {
+  var def, flat;
+
+  def = {
+    'default': {
+      name: 'my-form',
+      _elements: [
+        {
+          'default': {
+            name: 'element-1'
+          },
+          add: {
+            label: 'Element 1'
+          }
+        },
+        {
+          'default': {
+            name: 'element-2'
+          },
+          edit: {
+            label: 'Element 2'
+          }
+        },
+        {
+          'default': {
+            name: 'element-3'
+          },
+          list: {
+            label: 'Element 3'
+          }
+        }
+      ]
+    },
+    add: {
+      label: 'My Form',
+      _elements: [
+        'element-2',
+        'element-1'
+      ]
+    }
+  };
+  flat = variedDefinition.flatten(def, 'add');
+
+  t.test('output is an Object', function (tt) {
+    tt.isObject(flat);
+    tt.end();
+  });
+
+  t.test('properties in default are flattened as expected', function (tt) {
+    tt.isString(flat.label);
+    tt.equal(flat.label, def.add.label);
+    tt.end();
+  });
+
+  t.test('_elements listing in a variation controls order', function (tt) {
+    var original;
+    tt.lengthOf(flat._elements, 2);
+    original = def['default']._elements[1];
+    tt.isString(flat._elements[0].name);
+    tt.equal(flat._elements[0].name, original['default'].name);
+    original = def['default']._elements[0];
+    tt.isString(flat._elements[1].label);
+    tt.equal(flat._elements[1].label, original.add.label);
+    tt.end();
+  });
+
+  t.end();
+});
+
+test('precedence', function (t) {
+  var def;
+
+  def = {
+    'default': {
+      name: 'my-form',
+      string: 'My Form',
+      'boolean': true,
+      number: 1
+    },
+    add: {
+      string: '',
+      'boolean': false,
+      number: 0
+    },
+    edit: {
+      string: ' '
+    }
+  };
+
+  t.test('"add" Variation is flattened as expected', function (tt) {
+    var flat = variedDefinition.flatten(def, 'add');
+
+    tt.property(flat, 'name');
+    tt.isString(flat.name);
+    tt.equal(flat.name, def['default'].name);
+
+    // default.string is used because add.string is empty
+    tt.property(flat, 'string');
+    tt.isString(flat.string);
+    tt.equal(flat.string, def['default'].string);
+
+    tt.property(flat, 'boolean');
+    tt.isBoolean(flat['boolean']);
+    tt.equal(flat['boolean'], def.add['boolean']);
+
+    tt.property(flat, 'number');
+    tt.isNumber(flat.number);
+    tt.equal(flat.number, def.add.number);
+    tt.end();
+  });
+
+  t.test('"edit" Variation is flattened as expected', function (tt) {
+    var flat = variedDefinition.flatten(def, 'edit');
+
+    tt.property(flat, 'name');
+    tt.isString(flat.name);
+    tt.equal(flat.name, def['default'].name);
+
+    // edit.string is used because it is not empty
+    tt.property(flat, 'string');
+    tt.isString(flat.string);
+    tt.equal(flat.string, def.edit.string.trim());
+
+    tt.property(flat, 'boolean');
+    tt.isBoolean(flat['boolean']);
+    tt.equal(flat['boolean'], def['default']['boolean']);
+
+    tt.property(flat, 'number');
+    tt.isNumber(flat.number);
+    tt.equal(flat.number, def['default'].number);
+    tt.end();
+  });
+
+  t.end();
 });
